172a9bbc5160ab7cb73e9cc8137bac0fc0d2e201
Andrew <aschoch10@gmail.com>
2022-01-29 14:36:19 -0800

    added delete

--- client/src/components/ViewOne.jsx
+++ client/src/components/ViewOne.jsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import { useParams } from  'react-router-dom';
+import { useParams, useHistory } from  'react-router-dom';
 import axios from 'axios';
 
 const ViewOne = (props) => {
     //getting varibale from URL
+    let history = useHistory()
     const {id} = useParams()
     const [oneProduct, setOneProduct] = useState({})
+    const [products, setProducts] = useState([]);
 
     useEffect(()=>{
         axios.get("http://localhost:2222/api/products/" + id)
@@ -16,10 +18,21 @@ const ViewOne = (props) => {
         .catch(err => console.log(err))
     }, [])
 
+    const deleteProduct = (productId) => {
+        // console.log(productId);
+        axios.delete('http://localhost:2222/api/products/' + productId)
+            .then(res => {
+                console.log("Succesful deletion!");
+                history.push("/")
+            })
+            .catch(err => console.error(err));
+    }
+
     return <div>
             <h2>{oneProduct.title}</h2>
             <h3>{oneProduct.price}</h3>
             <h3>{oneProduct.description}</h3>
+            <h3><button onClick={() => { deleteProduct(oneProduct._id) }} className="btn btn-danger">Delete</button></h3>
     </div>;
 };
 
